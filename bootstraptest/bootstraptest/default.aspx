d1a99615a8dbc6fd98d918995a2f05702ad30c7a
chmodder <hotmailgateway@gmail.com>
2013-09-30 14:10:38 +0200

    fixed navigationlinks

--- bootstraptest/bootstraptest/default.aspx
+++ bootstraptest/bootstraptest/default.aspx
@@ -1,6 +1,6 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/masterpage.Master" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="bootstraptest._default1" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/masterpage.Master" AutoEventWireup="true" CodeBehind="default.aspx.cs"  %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <asp:SqlDataSource ID="SqlDataBrugere" runat="server"></asp:SqlDataSource>
+    
 </asp:Content>
--- bootstraptest/bootstraptest/default.aspx.designer.cs
+++ bootstraptest/bootstraptest/default.aspx.designer.cs
@@ -7,18 +7,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace bootstraptest {
-    
-    
-    public partial class _default1 {
-        
-        /// <summary>
-        /// SqlDataBrugere control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataBrugere;
-    }
+
+
+public partial class brugere {
 }
